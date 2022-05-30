02fddf08c53c07eea6d42e9ca96ea85064cceca2
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-30 15:44:06 -0300

    printinfos

---
+++ printInfos.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome: 'andre',
+    idade: 36,
+    cpf: '123456789',
+    email: 'andre@gmail.com'
+}
+
+console.log(cliente.nome)
+
+console.log(cliente.cpf.substring(0,1))
