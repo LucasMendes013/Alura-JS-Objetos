de4bd53899aaab8db08ae35ebc88e580d373bc89
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-30 15:31:02 -0300

    Objetos
    É uma forma abstraída de representar dados, eles sao conjuntos, diferentemente dos Arrays que ao serem passados, eram todos da mesma classe:
    cpfs:
    [121212][1212121]
    
    já em objetos usamos:
    const cadastro = {
        nome: 'andre',
        idade: 36,
        cpf: '123456789',
        email: 'andre@gmail.com'

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const listaCPFs = ["123456789", '012912912', '121213123']
+
+// const cliente = ['nome', 'andre', 'idade', 36]
+
+const cliente = {
+    nome: 'andre',
+    idade: 36,
+    cpf: '123456789',
+    email: 'andre@gmail.com'
+}
