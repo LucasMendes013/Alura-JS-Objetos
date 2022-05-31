5a7c061e4091d6d04593a51605bb52899c38900c
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-31 10:08:05 -0300

    prototipacao

---
+++ clienteGenerico.js
@@ -0,0 +1,13 @@
+function cliente(nome, cpf, email, saldo){
+    this.nome = nome
+    this.cpf = cpf
+    this.email = email
+    this.saldo = saldo
+    this.depositar = function(valor){
+        this.saldo += valor
+    }
+}
+
+const andre = new cliente('andre', '123123123', 'andre@gmail.com', 100)
+
+console.log(andre)
