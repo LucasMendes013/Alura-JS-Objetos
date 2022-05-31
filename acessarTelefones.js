7959228a587874cb3ea08592c087e855170118c8
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-31 08:24:08 -0300

    Trabalhando com arrays dentro de objetos
    e imprimindo a informacao no console.log

---
+++ acessarTelefones.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: 'andre',
+    idade: 36,
+    cpf: '123456789',
+    email: 'andre@gmail.com',
+    fone: ["55555555", '666666666']
+}
+
+cliente.fone.forEach(fone => console.log(fone))
